8ad4c56c9e61d5aaeebdd9722e4457c8f28f604b
atakantekoglu <atakantekoglu@gmail.com>
2022-10-21 11:02:28 +0300

    [feature]: price information and undefined property check

--- src/components/card/CartItem.jsx
+++ src/components/card/CartItem.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {Link} from "react-router-dom";
 
-function CartItem({ id, imageUrl, name, startDate, time, promoter }){
+function CartItem({ id, imageUrl, name, startDate, time, promoter, price }){
     return(
         <div className="card" style={{width: 'auto'}}>
             <img src={imageUrl} className="card-img-top" alt="event image" width={305} height={225}/>
@@ -14,9 +14,11 @@ function CartItem({ id, imageUrl, name, startDate, time, promoter }){
                     <span><b>Time:</b>{time}</span>
                 </li>
                 <li className="list-group-item">
+                    <span className="me-2"><b>Price:</b>{price}</span>
+                </li>
+                <li className="list-group-item">
                     <span>{promoter}</span>
                 </li>
-                <li className="list-group-item">A third item</li>
             </ul>
             <div className="card-body">
                 <Link to={`event-details/${id}`}>Details</Link>
